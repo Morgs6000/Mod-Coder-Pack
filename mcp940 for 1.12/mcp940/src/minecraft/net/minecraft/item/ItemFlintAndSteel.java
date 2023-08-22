f3029e025681e26ab5798ea886d056dc93c098be
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2023-08-22 13:50:45 -0300

    Update Commit

--- mcp940 for 1.12/mcp940/src/minecraft/net/minecraft/item/ItemFlintAndSteel.java
+++ mcp940 for 1.12/mcp940/src/minecraft/net/minecraft/item/ItemFlintAndSteel.java
@@ -14,8 +14,10 @@ import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class ItemFlintAndSteel extends Item {
-    public ItemFlintAndSteel() {
+public class ItemFlintAndSteel extends Item
+{
+    public ItemFlintAndSteel()
+    {
         this.maxStackSize = 1;
         this.setMaxDamage(64);
         this.setCreativeTab(CreativeTabs.TOOLS);
@@ -24,20 +26,25 @@ public class ItemFlintAndSteel extends Item {
     /**
      * Called when a Block is right-clicked with this Item
      */
-    public EnumActionResult onItemUse(EntityPlayer stack, World playerIn, BlockPos worldIn, EnumHand pos, EnumFacing hand, float facing, float hitX, float hitY) {
+    public EnumActionResult onItemUse(EntityPlayer stack, World playerIn, BlockPos worldIn, EnumHand pos, EnumFacing hand, float facing, float hitX, float hitY)
+    {
         worldIn = worldIn.offset(hand);
         ItemStack itemstack = stack.getHeldItem(pos);
 
-        if (!stack.canPlayerEdit(worldIn, hand, itemstack)) {
+        if (!stack.canPlayerEdit(worldIn, hand, itemstack))
+        {
             return EnumActionResult.FAIL;
         }
-        else {
-            if (playerIn.getBlockState(worldIn).getMaterial() == Material.AIR) {
+        else
+        {
+            if (playerIn.getBlockState(worldIn).getMaterial() == Material.AIR)
+            {
                 playerIn.playSound(stack, worldIn, SoundEvents.ITEM_FLINTANDSTEEL_USE, SoundCategory.BLOCKS, 1.0F, itemRand.nextFloat() * 0.4F + 0.8F);
                 playerIn.setBlockState(worldIn, Blocks.FIRE.getDefaultState(), 11);
             }
 
-            if (stack instanceof EntityPlayerMP) {
+            if (stack instanceof EntityPlayerMP)
+            {
                 CriteriaTriggers.field_193137_x.func_193173_a((EntityPlayerMP)stack, worldIn, itemstack);
             }
 
